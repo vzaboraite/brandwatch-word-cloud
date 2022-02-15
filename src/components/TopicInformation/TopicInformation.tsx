d3e95038cbd5a5961dc0251488327aad1b658d40
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2022-02-15 21:22:02 +0000

    Style TopicInformation component

--- src/components/TopicInformation/TopicInformation.tsx
+++ src/components/TopicInformation/TopicInformation.tsx
@@ -5,20 +5,20 @@ export default function TopicInformation({ topic }: { topic: Topic | null }) {
     <div className="topic-information">
       {topic !== null && (
         <>
-          <h2 className="information">Information on topic "{topic.label}"</h2>
+          <h2 className="information">Information on topic "{topic.label}":</h2>
           <p className="volume">Total Mentions: {topic.volume}</p>
           <ul className="sentiment">
             <li>
               Positive Mentions:
-              <span className="positive">{topic.sentiment.positive ?? 0}</span>
+              <span className="positive"> {topic.sentiment.positive ?? 0}</span>
             </li>
             <li>
               Neutral Mentions:
-              <span className="neutral">{topic.sentiment.neutral ?? 0}</span>
+              <span className="neutral"> {topic.sentiment.neutral ?? 0}</span>
             </li>
             <li>
               Negative Mentions:
-              <span className="negative">{topic.sentiment.negative ?? 0}</span>
+              <span className="negative"> {topic.sentiment.negative ?? 0}</span>
             </li>
           </ul>
         </>
